a0d01249136f641052f30e3b0a07d0a5bc7af5c8
Bigjek <ivapira@mail.ru>
2018-06-14 17:01:49 +0300

    add test propTypes

--- src/component/Header.js
+++ src/component/Header.js
@@ -1,16 +1,38 @@
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 class Header extends Component {
-  render() {
-      console.log('items', this.props.items);
-    return (
-      <div>
-        {this.props.items.map((item, index) => (
-            <a href={item.link} key={index}>{item.label}</a>
-        ))}
-      </div>
-    );
-  }
+    // проверка данных которые приходят из props (массив, обязательный)
+    // Вы можете ограничить значение свойства списком значений(oneOf)
+    // Ограничение, что свойство должно быть массивом значений определенного типа(arrayOf)
+    // Ограничение, что свойство должно быть объектом определенной формы(shape)
+    static propTypes = {
+        items: PropTypes.array.isRequired,
+        isLoading: PropTypes.bool,
+        submit: PropTypes.func.isRequired,
+        title: PropTypes.string.isRequired,
+        type: PropTypes.oneOf(['news', 'photos']),
+        user: PropTypes.shape({
+            name: PropTypes.string,
+            age: PropTypes.number
+        }),
+        users: PropTypes.arrayOf(
+            PropTypes.shape({
+                name: PropTypes.string,
+                age: PropTypes.number
+            })
+        )
+    }
+
+    render() {
+        console.log('items', this.props.items);
+        return (
+            <div>
+                {this.props.items.map((item, index) => (
+                    <a href={item.link} key={index}>{item.label}</a>
+                ))}
+            </div>
+        );
+    }
 }
 
 export default Header;
